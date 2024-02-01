b26d156ad342517b1576b06b8bfb43670a1e11ac
kowshik jallipalli <119750794+kowshikjallipalli0105@users.noreply.github.com>
2024-02-01 19:18:57 +0530

    import java.util.*; public class kowshik{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         double i=sc.nextInt();         System.out.printf("%.2f",i*2.54);     } }

---
+++ Inches_to_Centimeters.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class kowshik{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        double i=sc.nextInt();
+        System.out.printf("%.2f",i*2.54);
+    }
+}
