68129292a61ba362130f1a35b8cb95f78123c1f7
kowshik jallipalli <119750794+kowshikjallipalli0105@users.noreply.github.com>
2024-02-01 21:49:25 +0530

    import java.util.*; public class kowshik{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         double r =sc.nextDouble();         System.out.printf("%.2f",3.14*r*r);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class kowshik{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        double r =sc.nextDouble();
+        System.out.printf("%.2f",3.14*r*r);
+    }
+}
