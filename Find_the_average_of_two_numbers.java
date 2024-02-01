50285c7236491ff7390730e22453b58246852988
kowshik jallipalli <119750794+kowshikjallipalli0105@users.noreply.github.com>
2024-02-01 22:03:19 +0530

    import java.util.*; public class kowshik{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         double a=sc.nextDouble(),b=sc.nextDouble();         System.out.printf("%.4f",(a+b)/2);     } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class kowshik{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        double a=sc.nextDouble(),b=sc.nextDouble();
+        System.out.printf("%.4f",(a+b)/2);
+    }
+}
