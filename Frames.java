ab4bcf3dea38eb2ea211e51226c1442698f1cacf
kowshik jallipalli <119750794+kowshikjallipalli0105@users.noreply.github.com>
2024-02-01 18:40:47 +0530

    import java.util.*; public class demo{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt(),b=sc.nextInt(),c=sc.nextInt();         System.out.println(c*(2*(a+b)));     } }

---
+++ Frames.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class demo{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt(),b=sc.nextInt(),c=sc.nextInt();
+        System.out.println(c*(2*(a+b)));
+    }
+}
