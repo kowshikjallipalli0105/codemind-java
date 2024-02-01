cb555751243c581b7c0702423b568e6adb0cb7cd
kowshik jallipalli <119750794+kowshikjallipalli0105@users.noreply.github.com>
2024-02-01 21:10:12 +0530

    import java.util.*; public class kowshik{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt(),b=sc.nextInt(),c=sc.nextInt();         System.out.print(((a*5)+(b*10))/c);     } }

---
+++ Romeo_and_Juliet.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class kowshik{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt(),b=sc.nextInt(),c=sc.nextInt();
+        System.out.print(((a*5)+(b*10))/c);
+    }
+}
