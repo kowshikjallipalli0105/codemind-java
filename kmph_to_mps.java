a81b728ca151a86ab6075153f5575daec0a8de2c
kowshik jallipalli <119750794+kowshikjallipalli0105@users.noreply.github.com>
2024-02-01 19:04:23 +0530

    import java.util.*; public class demo{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         double km=sc.nextDouble();       System.out.printf("%.2f",5.0/18*km);     } }

---
+++ kmph_to_mps.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class demo{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        double km=sc.nextDouble();
+      System.out.printf("%.2f",5.0/18*km);
+    }
+}
