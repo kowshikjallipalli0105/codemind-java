eb1aad5a33fe70ff024e8862e58bfe5b8c534192
kowshik jallipalli <119750794+kowshikjallipalli0105@users.noreply.github.com>
2024-02-01 20:48:24 +0530

    import java.util.*; public class kowshik{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int d=sc.nextInt();         System.out.println(d/365);         System.out.println((d%365)/7);
        }
    }

---
+++ Days_into_years,_weeks:.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class kowshik{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int d=sc.nextInt();
+        System.out.println(d/365);
+        System.out.println((d%365)/7);
+
+    }
+}
