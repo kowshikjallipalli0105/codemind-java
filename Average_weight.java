53586993644716d749b59cd3786dd877738b743e
kowshik jallipalli <119750794+kowshikjallipalli0105@users.noreply.github.com>
2024-02-01 20:41:22 +0530

    import java.util.*; public class kowshik{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt(),b1=sc.nextInt(),b2=sc.nextInt();         System.out.println(3*a-b1-b2);     } }

---
+++ Average_weight.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class kowshik{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt(),b1=sc.nextInt(),b2=sc.nextInt();
+        System.out.println(3*a-b1-b2);
+    }
+}
