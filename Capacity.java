9011b983f880c56e2f9f94e61c3230a44e2d7c15
kowshik jallipalli <119750794+kowshikjallipalli0105@users.noreply.github.com>
2024-02-01 20:33:15 +0530

    import java.util.*; public class kowshik{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt(),b=sc.nextInt(),c=sc.nextInt();         System.out.println(((2*a*b*c*512)/1024)+" KB");     } }

---
+++ Capacity.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class kowshik{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt(),b=sc.nextInt(),c=sc.nextInt();
+        System.out.println(((2*a*b*c*512)/1024)+" KB");
+    }
+}
