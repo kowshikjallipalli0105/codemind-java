94f9c34e7a9760f43bc7eccf9ad93ad16da77614
kowshik jallipalli <119750794+kowshikjallipalli0105@users.noreply.github.com>
2024-02-01 18:56:16 +0530

    import java.util.*; public class demo{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         double a= sc.nextInt();         System.out.printf("%.2f",(4.0/3)*3.14*a*a*a);     } }

---
+++ Volume_of_Sphere.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class demo{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        double a= sc.nextInt();
+        System.out.printf("%.2f",(4.0/3)*3.14*a*a*a);
+    }
+}
