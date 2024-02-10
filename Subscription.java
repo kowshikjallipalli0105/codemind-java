6e503611d90e29209305ae628b375f473b49db8d
kowshik jallipalli <119750794+kowshikjallipalli0105@users.noreply.github.com>
2024-02-10 22:06:39 +0530

    import java.util.*; public class kowshik{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int s=sc.nextInt();         System.out.println(s<=40?"NO":"YES");     } }

---
+++ Subscription.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class kowshik{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int s=sc.nextInt();
+        System.out.println(s<=40?"NO":"YES");
+    }
+}
