10b5abae1338ead99158da949ec9633cb2484df3
kowshik jallipalli <119750794+kowshikjallipalli0105@users.noreply.github.com>
2024-02-10 21:00:02 +0530

    import java.util.*; public class kowshik{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         float  a=sc.nextFloat(),b=sc.nextFloat();         System.out.printf("%.2f",a*b);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class kowshik{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        float  a=sc.nextFloat(),b=sc.nextFloat();
+        System.out.printf("%.2f",a*b);
+    }
+}
