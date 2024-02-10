8c93d83f8511025339470e34a381ce526a0ba8d3
kowshik jallipalli <119750794+kowshikjallipalli0105@users.noreply.github.com>
2024-02-10 22:01:46 +0530

    import java.util.*; public class kowshik{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int t=sc.nextInt();         System.out.println(t>98?"YES":"NO");     } }

---
+++ Fever.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class kowshik{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        System.out.println(t>98?"YES":"NO");
+    }
+}
