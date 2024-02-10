9fe7af1703e5b57787511a6393579221a0a2dc53
kowshik jallipalli <119750794+kowshikjallipalli0105@users.noreply.github.com>
2024-02-10 21:51:37 +0530

    import java.util.*; public class kowshik{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int t=sc.nextInt();         while(t-->0){             int n=sc.nextInt(),m=sc.nextInt(),k=sc.nextInt();         System.out.println(n<=m-k?"YES":"NO");         }     } }

---
+++ Student_Registration.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class kowshik{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt(),m=sc.nextInt(),k=sc.nextInt();
+        System.out.println(n<=m-k?"YES":"NO");
+        }
+    }
+}
